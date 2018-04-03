741cb79848d935a280b04ff21219bafd2c92f267
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-03 17:04:00 +0900

    GenerateFile-for-ViewSettings

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   get 'news' => 'news#index'
   root 'news#index'
   resources :news
+
+  # idを含まない単数形のresource
+  resource :settings, only: [:index] do
+    collection do
+      get :profile ,:mail,:password,:subscriptionns,:mute,:social,:query,:newsletters,:scout
+    end
+  end
 end
