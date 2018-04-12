56180600fe07803578a3531c0a29a1d3f4f0f882
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 02:58:18 +0900

    add-rbenv-rehash

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/newspicks_23
 RAILS_ENV=production gem install bundler
+RAILS_ENV=production rbenv rehash
 RAILS_ENV=production bundle install --path vendor/bundle
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:clobber
