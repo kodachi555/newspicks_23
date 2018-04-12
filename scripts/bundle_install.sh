c7ecedf24eb59ce6651a19b4be40403df28b91b3
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 03:20:09 +0900

    recorrect

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /var/www/newspicks_23
-RAILS_ENV=production gem install bundler
-RAILS_ENV=production rbenv rehash
 RAILS_ENV=production bundle install --path vendor/bundle
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:clobber
