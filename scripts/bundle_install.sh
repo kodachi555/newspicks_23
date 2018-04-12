29fe880c5d393069736a33da27aab311a2dd004a
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 02:48:12 +0900

    add-gem-install-bundler

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/newspicks_23
+RAILS_ENV=production gem install bundler
 RAILS_ENV=production bundle install --path vendor/bundle
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:clobber
