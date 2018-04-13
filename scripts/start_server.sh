601cde96b041c659b17e672ca7d225d383bb3eea
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 14:21:35 +0900

    correct-command

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-deploy -c 'cd /var/www/newspicks_23 && bundle exec unicorn_rails -D -E production -c config/unicorn.rb'
-sudo service nginx restart
+deploy -c 'cd /var/www/newspicks_23 && RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -D -E production -c config/unicorn.rb'
