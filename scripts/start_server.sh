4776a476c4d7851bc7bcb0349394a825d27af6eb
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 09:47:54 +0900

    correct-add-command-path

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-su -l deploy -c 'cd /var/www/newspicks_23 && bundle exec unicorn -D -E production -c config/unicorn.rb'
+su -l deploy -c 'cd /var/www/newspicks_23 && /opt/rbenv/shims/bundle/bundle exec unicorn -D -E production -c config/unicorn.rb'
 sudo service nginx restart
