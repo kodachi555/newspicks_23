779a7e6164cf882a45065d72b26fbea36b247115
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 19:50:20 +0900

    correct-sudo-user

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-cd /var/www/newspicks_23 && RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -D -E production -c config/unicorn.rb
+cd /var/www/newspicks_23 && sudo -u ec2-user RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -D -E production -c config/unicorn.rb
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill -KILL -s QUIT `cat /var/www/newspicks_23/tmp/pids/unicorn.pid`
+sudo kill -KILL -s QUIT `cat /var/www/newspicks_23/tmp/pids/unicorn.pid`
