ae1e260e21e2d2f9fa05490497a5dd84c6c9bba7
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 10:42:34 +0900

    correct-gem-unicorn-dbMysql

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-su -l deploy -c 'cd /var/www/newspicks_23 && bundle exec unicorn -D -E production -c config/unicorn.rb'
+su -l deploy -c 'cd /var/www/newspicks_23 && bundle exec unicorn_rails -D -E production -c config/unicorn.rb'
 sudo service nginx restart
