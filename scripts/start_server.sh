ef63276711e8b53d2b3e3630a54a86a55581f8c5
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 14:57:33 +0900

    correct-unicorn_rails-erase-D

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-cd /var/www/newspicks_23 && RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -D -E production -c config/unicorn.rb
+cd /var/www/newspicks_23 && RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -E production -c config/unicorn.rb
