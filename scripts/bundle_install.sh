bd5f4cfec2fd1678443f867265a01c0b144c2dde
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-13 09:54:56 +0900

    correction-add-source

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /var/www/newspicks_23
-RAILS_ENV=production /opt/rbenv/shims/bundle/bundle install --path vendor/bundle
-RAILS_ENV=production /opt/rbenv/shims/bundle/bundle exec rake db:migrate
-RAILS_ENV=production /opt/rbenv/shims/bundle/bundle exec rake assets:clobber
-RAILS_ENV=production /opt/rbenv/shims/bundle/bundle exec rake assets:precompile
+RAILS_ENV=production bundle install --path vendor/bundle
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:clobber
+RAILS_ENV=production bundle exec rake assets:precompile
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-su -l deploy -c 'cd /var/www/newspicks_23 && /opt/rbenv/shims/bundle/bundle exec unicorn -D -E production -c config/unicorn.rb'
+source /home/ec2-user/.bash_profile
+su -l deploy -c 'cd /var/www/newspicks_23 && bundle exec unicorn -D -E production -c config/unicorn.rb'
 sudo service nginx restart
