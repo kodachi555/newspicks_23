8fbce2d041e6882652e7c10f986ee7038b5efa2e
ryuji0000 <info.ryuji.0000@gmail.com>
2018-04-16 15:35:26 +0900

    add_devise

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,9 @@
 class NewsController < ApplicationController
 
   def index
+    @products = Product.all
   end
-
+  def show
+    @product = Products.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'news' => 'news#index'
   root 'news#index'
   resources :news
+  resources :products
 end
