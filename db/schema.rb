549a6ef07344b70314f342ead10ca8ffe2cfe197
東條晃多 <tjkota1090@icloud.com>
2018-04-16 17:15:38 +0900

    Drop table news

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416075500) do
+ActiveRecord::Schema.define(version: 20180416081417) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
-  end
-
-  create_table "news", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title",       null: false
-    t.string   "image_url"
-    t.string   "page_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "site_name"
-    t.integer  "category_id"
-    t.index ["title"], name: "index_news_on_title", using: :btree
   end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
